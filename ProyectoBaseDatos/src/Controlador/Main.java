a0d7b5fd77a9dfdaca18745acb760fc24991d3da
Usuario <Usuario@DESKTOP-AQ7VM1K>
2023-03-23 10:21:38 -0300

    test 2

--- ProyectoBaseDatos/src/Controlador/Main.java
+++ ProyectoBaseDatos/src/Controlador/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Connection connection = DataBaseManager.getConnection();
-		System.out.println("Hola estoy es GIT");
+		System.out.println("Hola estoy es GITtttt");
 
 	}
 
