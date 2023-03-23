3cdd0c861fca3ed38418604843f9f55b70a1a51a
Usuario <Usuario@DESKTOP-AQ7VM1K>
2023-03-23 10:26:45 -0300

    test4

--- ProyectoBaseDatos/src/Controlador/Main.java
+++ ProyectoBaseDatos/src/Controlador/Main.java
@@ -9,6 +9,7 @@ public class Main {
 	public static void main(String[] args) {
 		Connection connection = DataBaseManager.getConnection();
 		System.out.println("abdlasdfalsdb");
+		System.out.println("holaaaaaaa");
 
 	}
 
